bc076ebf5f8d8005a22a348f14040c1c1f1935c8
Jason Leong <soundslikejason@users.noreply.github.com>
2016-12-21 16:12:31 -0800

    ajax settings values

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   function weather() {
     $.ajax({
-      url:,
-      type:,
-      dataType:,
-      success: function() {}
+      url: "http://ip-api.com/json",
+      type: 'GET',
+      dataType: 'jsonp',
+      success: function(data) {}
 
 
     });
