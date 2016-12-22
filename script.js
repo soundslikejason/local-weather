1745bab3999fd2818f2c18498b432685ce5e3e33
Jason Leong <soundslikejason@users.noreply.github.com>
2016-12-22 11:26:17 -0800

    get location coordinates

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
+
+  var latitude;
+  var longitude;
+
+  weather();
+
   function weather() {
     $.ajax({
       url: "http://ip-api.com/json",
       type: 'GET',
       dataType: 'jsonp',
-      success: function(data) {}
+      success: function(data) {
+        latitude = data.lat;
+        longitude = data.lon;
+        $ajax({
+          url:,
+          data:,
+          cache:,
+          dataType:,
+          success: function(data) {}
+        });
+      }
 
 
     });
